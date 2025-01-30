b122c213a2a454e3bf0bc4f4dfdc1204603ffa33
Akram Ewida Ahmed Ewida <info@quick4you.net>
2025-01-30 15:55:58 +0000

    added products details page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -13,3 +13,19 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details, including rated items """
+
+    product = get_object_or_404(Product, pk=product_id)
+    
+    # Fetch random rated products
+    rated_products = Product.objects.filter(rating__isnull=False).order_by('?')  # Fetch 6 random rated products
+
+    context = {
+        'product': product,
+        'rated_products': rated_products,
+    }
+
+    return render(request, 'products/product_detail.html', context)
