e6fcec4d33f2ad4a3cff98b300027f1681da17ee
akramalex <info@quick4you.net>
2025-02-03 18:40:09 +0000

    added sorting

--- products/views.py
+++ products/views.py
@@ -11,8 +11,23 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -27,12 +42,14 @@ def all_products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+    current_sorting = f'{sort}_{direction}'
 
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
@@ -48,8 +65,10 @@ def product_detail(request, product_id):
     discount_tiers = product.discounttier_set.all()  # Fetch all DiscountTier entries related to the product
 
     # Fetch random rated products (example: 6 random rated products)
-    rated_products = Product.objects.filter(rating__isnull=False).order_by('?')[:20]
-
+    rated_products = Product.objects.filter(
+        category=product.category,  # Match the category
+        rating__isnull=False       # Ensure they have a rating
+    ).exclude(id=product.id).order_by('?')[:20]  # Exclude the current product and limit results
     # Add all relevant data to the context
     context = {
         'product': product,
