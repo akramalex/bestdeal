7fde066af266fdb6cab0a9227afdfd1206b70e59
akramalex <info@quick4you.net>
2025-02-13 17:26:09 +0000

    compeleted webhook handler

--- checkout/models.py
+++ checkout/models.py
@@ -25,6 +25,8 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     vat = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     VAT_PERCENTAGE = Decimal('20.0')  # 20% VAT rate
 
