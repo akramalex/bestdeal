4c400aa407541f1e5ce070b80705d86f0c626d1e
akramalex <info@quick4you.net>
2025-02-19 14:30:30 +0000

    udpate setting

--- best_deal/settings.py
+++ best_deal/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sl$v6!57m0y=2mqpa7h5e_i#)$k6luu3&e0n9@h#z%t69h!yyc'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-akramalex-bestdeal-op1o6ypwc59.ws.codeinstitute-ide.net', 
                  '127.0.0.1', 'localhost','bestdeals-9ab4570971bc.herokuapp.com']
