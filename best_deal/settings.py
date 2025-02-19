9376bf8405b40bc15c95306d2f00b488517dd670
akramalex <info@quick4you.net>
2025-02-19 14:42:07 +0000

    Replace runtime.txt with .python-version

--- best_deal/settings.py
+++ best_deal/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-import dj_database_url
 if os.path.exists('env.py'):
     import env
+
+
+import dj_database_url
 
 from pathlib import Path
 
