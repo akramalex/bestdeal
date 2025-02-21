0cdd7e503ee985371421d808c3d04efe76aa637a
akramalex <info@quick4you.net>
2025-02-21 14:31:02 +0000

    change in settings file

--- best_deal/settings.py
+++ best_deal/settings.py
@@ -202,11 +202,17 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
      # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+     # Static and media files
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
+    STORAGES = {
+         "default": {
+                 "BACKEND": "custom_storages.MediaStorage",
+         },
+         "staticfiles": {"BACKEND": "custom_storages.StaticStorage"},
+}
 
+    MEDIAFILES_LOCATION = 'media'
+    
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
