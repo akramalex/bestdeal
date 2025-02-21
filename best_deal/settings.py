cc5e5fb404b5f9cf48e1b431b8c0fb06b56eaf32
akramalex <info@quick4you.net>
2025-02-21 16:13:10 +0000

    chanage amazon bucket and updated in setting

--- best_deal/settings.py
+++ best_deal/settings.py
@@ -196,7 +196,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'bestdeal-64124e15271c'
+    AWS_STORAGE_BUCKET_NAME = 'best-deal0'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
